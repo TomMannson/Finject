1a4724b7b927e1242ef57fab8c21b30ccd8802db
tomasz.krol <tomasz.krol88.dev@gmail.com>
2020-01-13 15:54:27 +0100

    implementation for extracting super class

--- example/lib/configuration.dart
+++ example/lib/configuration.dart
@@ -1,25 +1,22 @@
 import 'package:example/test.dart';
-import 'package:example/test2.dart' as a;
 import 'package:finject/finject.dart';
-
 
 @Configuration()
 class Config {
-
   @Named("one")
   @Scoped("test")
   @Singleton()
-  TestClass one(){
+  TestClass one() {
     return TestClass("one");
   }
 
   @Named("two")
   @Scoped("test")
-  TestClass two(){
+  TestClass two() {
     return TestClass("two");
   }
 
-  a.TestClass three(){
-    return a.TestClass("three");
+  String valueString() {
+    return "three";
   }
 }
