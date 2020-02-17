06a7df5d3f3ce6b32a92c2af87814e6e2d2210a8
Tommannson <tomasz.krol88@gmail.com>
2020-02-17 21:48:28 +0100

    new exports

--- finject_flutter/lib/finject_flutter.dart
+++ finject_flutter/lib/finject_flutter.dart
@@ -1,8 +1,11 @@
 library finject_flutter;
 
+export 'package:finject/finject.dart';
+
+export 'src/finject.dart';
 export 'src/finject_host.dart';
 export 'src/flat_inject_host.dart';
 export 'src/inject_host.dart';
 export 'src/injection_provider.dart';
 export 'src/scope_host.dart';
-export 'package:finject/finject.dart';
+export 'src/stateful/di_state.dart';
